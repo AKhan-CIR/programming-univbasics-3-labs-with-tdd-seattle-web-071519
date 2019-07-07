91eb9b74a8abbeb7adfe794a24d1bfcf79d1684c
Asif Khan <akhan@codeinreach.com>
2019-07-07 04:13:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,8 @@
 # Add your variables here
-fist_number = 1
+first_number = 1
 second_number = 2
 
-sum = fist_number + second_number
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
